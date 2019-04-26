6715bf5fefe775788ebbd85cf4cbd085f90d43e0
uthng <u.nguyenthanh@gmail.com>
2019-04-27 01:30:19 +0200

    feat(scaleway/ubuntu18.04-docker): Create new image for C2S
    - Create new image for Scaleway C2S servers
    - Use scaleway_ssh_key variable instead of hardcoding
    - Add name for each build

--- docker-install.sh
+++ docker-install.sh
@@ -19,7 +19,7 @@ add-apt-repository \
    stable"
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
--- sudo-configure.sh
+++ sudo-configure.sh
@@ -2,4 +2,7 @@
 
 set -x
 
+apt-get -y install sudo
+useradd -G sudo,docker uthng
+
 echo "%uthng ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/uthng
